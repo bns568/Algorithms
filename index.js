3119d908e9942e8b774e0ab8e7a145f48f44a760
‘Brittany <brittany.stubbs1827@gmail.com>
2018-07-10 19:05:39 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+    let reversedString = "";
+    for (int i=string.length; i > 0; i--){
+        reversedString += string[i];
+    }
+
+}
+
+module.exports = reverse;
